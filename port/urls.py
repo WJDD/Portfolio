73a518856a9f87ead8ff810853898eb94aa954c2
WJDD <475317147z@gmail.com>
2017-06-30 23:20:47 -0300

    complete first page

--- port/urls.py
+++ port/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^skill/$', views.skill, name='skill'),
-    url(r'^p&e/$', views.pe, name='p&e'),
 ]
--- port/views.py
+++ port/views.py
@@ -6,9 +6,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'port/home.html', {})
-
-def skill(request):
-    return render(request, 'port/skill.html', {})
-
-def pe(request):
-    return render(request, 'port/p&e.html', {})
